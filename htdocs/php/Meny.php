21c143368ca84fefcf6be7ef4da76c7f5e85a624
André Franzén <anfr01001@utb.vaxjo.se>
2020-03-25 10:40:58 +0100

    Man kan nu även logga ut
    Sista för denna lektionen. Inloggningyststem färdigt

--- htdocs/php/Meny.php
+++ htdocs/php/Meny.php
@@ -1,4 +1,7 @@
-<nav>
+<?php 
+
+$Meny1 = <<<HTML
+		<nav>
 	<ul>
 		<li><a href="index.php">Frågor</a></li>
 		<li><a href="yttranden.php">Yttranden</a></li>
@@ -9,5 +12,29 @@
 		</li>
 		
 	</ul>
-</nav>
+</nav>           
+HTML;
+
+$Meny2 = <<<HTML
+		<nav>
+	<ul>
+		<li><a href="index.php">Frågor</a></li>
+		<li><a href="yttranden.php">Yttranden</a></li>
+		<li><a href="logout.php">Logga ut</a></li>
+		<li>
+		<a href="profil.php"><img class="profilruta" src = "../html/bilder/teknikum.jpg" alt = "profilbild">Profil</a>
+			
+		</li>
 		
+	</ul>
+</nav>           
+HTML;
+
+if (isset($_SESSION['username'])) {
+	echo $Meny2;
+} else {
+	echo $Meny1;
+}
+		
+
+?>
