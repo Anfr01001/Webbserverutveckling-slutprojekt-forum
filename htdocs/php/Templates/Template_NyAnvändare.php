e5c2fff63910733389f4e0d2f384cdcacc840aee
André Franzén <anfr01001@utb.vaxjo.se>
2020-03-25 10:31:00 +0100

    Man kan nu ladda upp filer(bilder) som profilbild
    Koden för uppladdning av biler är tagen från W3 schools

--- htdocs/php/Templates/Template_NyAnvändare.php
+++ htdocs/php/Templates/Template_NyAnvändare.php
@@ -18,6 +18,53 @@ $picturre = filter_input(INPUT_POST,'picturre', FILTER_SANITIZE_STRING,FILTER_FL
 $passwordnew  = password_hash($passwordnew, PASSWORD_DEFAULT);
 
 if(!empty($usernamenew) && !empty($passwordnew) ) {
+
+$target_dir = "bilder/"; 
+
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+	
+
+	
 	//kollar inte efter profilbild eller bio då de kan vara tomma
 	include"Connect.php";
 
@@ -33,12 +80,13 @@ if(!empty($usernamenew) && !empty($passwordnew) ) {
 		//Ingen användare med namn skapar en ny
 	 $sql="INSERT INTO users(username,password,bio,profilbild,status) VALUE(?,?,?,?,?)";
 	 $res = $dbh->prepare($sql);
-	 $res->bind_param("ssssi", $usernamenew, $passwordnew, $bio, $picturre, $status);
+	 $res->bind_param("ssssi", $usernamenew, $passwordnew, $bio, $target_file, $status);
 	 $res->execute();
 		
 	 header("location:login.php");
 	} else {
 		header("location:NyAnvändare.php?value=1");
+
 	}
 }
 ?>
@@ -61,15 +109,15 @@ if(!empty($usernamenew) && !empty($passwordnew) ) {
 				<main> <!--Huvudinnehåll-->
 				<section>
 				<?php echo $str ?>
-					 <form action="NyAnvändare.php" method="post">
+					 <form action="NyAnvändare.php" method="post" enctype="multipart/form-data">
             <p><label for="user">Användarnamn:</label>
             <input type="text" id="user" name="usernamenew"></p>
             <p><label for="pwd">Lösenord:</label>
             <input type="password" id="pwd" name="passwordnew"></p>
 			<p><label for="user">Biografi:</label>
             <input type="text" id="bio" name="bio"></p>
-			<p><label for="user">Profilbild:</label>
-            <input type="text" id="pic" name="picturre"></p>
+			<p><label for="Bild">Bild:</label>
+            <input type="file" name="fileToUpload" id="fileToUpload"></p>
             <p>
             <input type="submit" value="Skapa användare">
             </p>
