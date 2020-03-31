031596a5a46921f73e07bdb0312da4c983740f2c
André Franzén <anfr01001@utb.vaxjo.se>
2020-03-31 10:32:12 +0200

    Påbörjat forumsidan (grund för inlägg)
    Jag har börjat med strukturen av hur sidan ska se ut och lite funktionalitet. inlägg importeras från databasen men man kan inte ännu lägga upp själv

--- htdocs/php/Templates/Template_index.php
+++ htdocs/php/Templates/Template_index.php
@@ -1,3 +1,14 @@
+<?php
+
+include"Connect.php";
+$sql= "SELECT * FROM posts"; 
+	$res = $dbh->prepare($sql);
+	$res->execute();
+	$result =$res->get_result();
+	
+	
+
+?>
 <!DOCTYPE html>
 <html lang="sv">
   <head>
@@ -14,10 +25,66 @@
 				 require "../php/Meny.php";
 				 require "../php/Header.php";
 				?>
-				
-				<br>
-				<br>
-				<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			
+				<table>
+  <tr>
+    <th>Inlägg</th>
+    <th>Skapare</th>
+  </tr>
+  
+  <?php
+  while($row = $result->fetch_assoc()) {
+	  //tar eda på vem som postat (med userID)
+	$sql2= "SELECT username FROM users WHERE UserID=?"; 
+	$res2 = $dbh->prepare($sql2);
+	$res2->bind_param("i", $row['skapare']);
+	$res2->execute();
+	$result2 =$res2->get_result();
+	$name = $result2->fetch_assoc();
+	$name = $name['username'];
+	
+	
+	echo "<tr> <td>";
+	echo $row['titel'];
+	echo "</td></tr>";
+	echo "<tr>";
+	echo "<td>";
+    echo $row['text'];
+	echo "</td>";
+    echo "<td>";
+	echo $name;
+	echo "</td>";
+  echo"
+  </tr>
+  <tr>
+  <td>Kommentarsfält</td>
+  </tr>";
+	}
+	
+	echo "<tr> <td>";
+	echo "Skapa nytt inlägg";
+	echo "</td></tr>";
+	
+	echo <<<NyPost
+<form action="index.php" method="post">
+	<tr> <td>
+	<p><label for="text">Titel:</label>
+	<input type="text" id="Titel" name="Titel">
+	</td></tr>
+	<tr> <td>
+    <p><label for="text">Text:</label>
+	<input type="text" id="Text" name="Text">
+	</td>
+    <td>
+	<input type="submit" value="Skapa inlägg">
+	</td>
+NyPost;
+ 
+ 
+ 
+ ?>
+</table>
+			
 			</main>
 		</div>
 
